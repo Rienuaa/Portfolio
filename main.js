9e158b57a0e6f361edc65523d74ce00c2e07b8e4
Q <aaacantor@gmail.com>
2023-05-30 14:40:02 -0400

    Added stats tracking

--- main.js
+++ main.js
@@ -78,7 +78,7 @@ function CheckBoxCommentary(checkboxElement)
   var commentary = document.getElementsByTagName("li");
   for ( var i = 0; i < commentary.length; i++ )
   {
-    if (commentary[i].className != "commentary")
+    if ( commentary[i].className != "commentary" )
     {
       if ( IsChecked )
       {
@@ -90,4 +90,127 @@ function CheckBoxCommentary(checkboxElement)
       }
     }
   }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    ShowStats();
+}, false);
+
+function ShowStats()
+{
+  let headlines = GetTotalHeadlineFeatures();
+  let balance = GetTotalBalanceChanges();
+  let bugs = GetTotalBugfixes();
+  let engineering = GetTotalEngineeringChanges();
+  
+  document.getElementById("FeatureLabel").innerHTML = "Headline Features (" + headlines + ")";
+  document.getElementById("BalanceLabel").innerHTML = "Balance Changes and Small Features (" + balance + ")";
+  document.getElementById("BugfixLabel").innerHTML = "Bug Fixes (" + bugs + ")";
+  document.getElementById("EngineeringLabel").innerHTML = "Engineering Work (" + engineering + ")";
+}
+
+function GetTotalEntries()
+{
+  let headline = GetTotalHeadlineFeatures();
+  let balance = GetTotalBalanceChanges();
+  let bugs = GetTotalBugfixes();
+  let engineering = GetTotalEngineeringChanges();
+  
+  return headline + balance + bugs + engineering;
+}
+
+function GetTotalHeadlineFeatures()
+{
+  var features = document.getElementsByClassName("features");
+  
+  let total = 0;
+  
+  for ( var i = 0; i < features.length; i++ )
+  {
+    var children = features[i].children;
+    
+    for ( var j = 0; j < children.length; j++ )
+    {
+      var child = children[j];
+
+      if ( child.nodeName == "UL" )
+      {
+        total += child.childElementCount;
+      }
+    }
+  }
+
+  return total;
+}
+
+function GetTotalBalanceChanges()
+{
+  var balance = document.getElementsByClassName("balance");
+  
+  let total = 0;
+  
+  for ( var i = 0; i < balance.length; i++ )
+  {
+    var children = balance[i].children;
+    
+    for ( var j = 0; j < children.length; j++ )
+    {
+      var child = children[j];
+
+      if ( child.nodeName == "UL" )
+      {
+        total += child.childElementCount;
+      }
+    }
+  }
+
+  return total;
+}
+
+function GetTotalBugfixes()
+{
+  var bugfixes = document.getElementsByClassName("bugfixes");
+  
+  let total = 0;
+  
+  for ( var i = 0; i < bugfixes.length; i++ )
+  {
+    var children = bugfixes[i].children;
+    
+    for ( var j = 0; j < children.length; j++ )
+    {
+      var child = children[j];
+
+      if ( child.nodeName == "UL" )
+      {
+        total += child.childElementCount;
+      }
+    }
+  }
+
+  return total;
+}
+
+function GetTotalEngineeringChanges()
+{
+  var engineering = document.getElementsByClassName("engineering");
+  
+  let total = 0;
+  
+  for ( var i = 0; i < engineering.length; i++ )
+  {
+    var children = engineering[i].children;
+    
+    for ( var j = 0; j < children.length; j++ )
+    {
+      var child = children[j];
+
+      if ( child.nodeName == "UL" )
+      {
+        total += child.childElementCount;
+      }
+    }
+  }
+
+  return total;
 }
